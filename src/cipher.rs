91d582e66d224bba3148246b6e06d130471f2dca
Max <max_coy612@outlook.com>
2025-08-23 21:36:18 -0500

    Initial AES code skeleton

---
+++ src/cipher.rs
@@ -0,0 +1,23 @@
+pub fn AES_encyrpt() {
+    unimplemented!();
+}
+
+pub fn AES_decrypt() {
+    unimplemented!();
+}
+
+fn sub_bytes() {
+    unimplemented!();
+}
+
+fn shift_rows() {
+    unimplemented!();
+}
+
+fn mix_columns() {
+    unimplemented!();
+}
+
+fn add_round_key() {
+    unimplemented!();
+}
