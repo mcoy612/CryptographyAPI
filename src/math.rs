4bee9259721e702b7c63aa3a56564c39720c4021
Max <max_coy612@outlook.com>
2025-09-18 21:04:48 -0500

    Added more byte arithmetic operations

--- src/math.rs
+++ src/math.rs
@@ -1,4 +1,4 @@
-static AES_IRREDUCIBLE_POLYNOMIAL: u16 = 0b_1_0001_1011;
+pub const AES_IRREDUCIBLE_POLYNOMIAL: u16 = 0b_1_0001_1011;
 
 /// Description:
 /// Multiplies two bytes assuming the bytes are elements of 2Z[x]/p(x) where p(x) is the irreducible polynomial for AES.
@@ -25,15 +25,128 @@ pub fn byte_multiplication(a: u8, b: u8) -> u8 {
         digit1 = digit1 >> 1;
     }
 
-    // Get remainder in 2Z[x]/p(x)
-    for i in (8..16).rev() {
-        let digit = (product >> i) & 1; // Get ith bit
+    byte_remainder(product, AES_IRREDUCIBLE_POLYNOMIAL) as u8
+}
+
+/// Description:
+/// Finds the quotient of a when divided by b. 
+/// Finds q in a = bq + r.
+///
+/// Arguments:
+/// a - (I,REQ) - dividend
+/// b - (I,REQ) - divisor
+///
+/// Returns:
+/// floor of a/b
+pub fn byte_quotient(a: u16, b: u16) -> u16 {
+    assert!(b != 0, "Can't divide by 0");
+
+    let mut degree: u16 = 0;
+    while b >> degree+1 != 0 {
+        degree += 1;
+    }
+
+    let mut a = a;
+    let mut res = 0;
+    for i in (degree..16).rev() {
+        let digit = (a >> i) & 1; // Get ith bit
         if digit == 1 {
-            product ^= AES_IRREDUCIBLE_POLYNOMIAL << (i-8);
+            res ^= digit << (i-degree);
+            a ^= b << (i-degree);
         }
     }
 
-    product as u8
+    res
+}
+
+/// Description:
+/// Finds the remainder of a when divided by b.
+/// Finds r in a = bq + r.
+/// 
+/// Arguments:
+/// a - (I,REQ) - dividend
+/// b - (I,REQ) - divisor
+///
+/// Returns:
+/// a modulo b
+pub fn byte_remainder(a: u16, b: u16) -> u16 {
+    assert!(b != 0, "Can't divide by 0");
+
+    let mut degree: u16 = 0;
+    while b >> degree+1 != 0 {
+        degree += 1;
+    }
+
+    let mut a = a;
+    for i in (degree..16).rev() {
+        let digit = (a >> i) & 1; // Get ith bit
+        if digit == 1 {
+            a ^= b << (i-degree);
+        }
+    }
+
+    a
+}
+
+/// Description:
+/// Does matrix multiplication of a 4x4 matrix of bytes with a vector of bytes of size 4.
+///
+/// Arguments:
+/// A - (I,REQ) - the matrix
+/// B - (I,REQ) - the vector
+///
+/// Returns:
+/// A * b
+pub fn byte_matrix_multiplication(A: &[[u8; 4]; 4], b: &[u8; 4]) -> [u8; 4] {
+    let mut res: [u8; 4] = [0; 4];
+    for i in 0..4 {
+        let mut sum: u8 = 0;
+        for j in 0..4 {
+            sum ^= byte_multiplication(A[i][j], b[j])
+        }
+        res[i] = sum;
+    }
+
+    res
+}
+
+/// Description:
+/// Find b such that ab=1.
+///
+/// Arguments:
+/// a - (I,REQ) - The number of find the inverse of.
+/// 
+/// Constraints:
+/// a must be non-zero.
+///
+/// Returns:
+/// The inverse of a.
+pub fn byte_inverse(a: u8) -> u8 {
+    assert!(a != 0, "0 does not have an inverse");
+
+    let mut p = AES_IRREDUCIBLE_POLYNOMIAL;
+    let mut a = a as u16;
+    let mut combinations: Vec<u8> = Vec::new(); // (q,r) for each step of calculating GCD
+
+    // Extended euclidean algorithm
+    while a != 0 {
+        let q = byte_quotient(p, a) as u8;
+        let r = byte_remainder(p, a) as u8;
+        p = a;
+        a = r as u16;
+        combinations.push(q);
+    }
+
+    let mut u = 0;
+    let mut v = 1;
+    combinations.pop();
+    while let Some(q) = combinations.pop() {
+        let temp = v;
+        v = u ^ byte_multiplication(q, v);
+        u = temp;
+    }
+
+    v
 }
 
 
@@ -59,6 +172,49 @@ mod tests {
         let b: u8 = 0b_0000_0001;
         let res = byte_multiplication(a, b);
         let actual: u8 = 0b_1111_1111;
+        assert_eq!(res, actual);
+    }
+
+    #[test]
+    fn byte_quotient_test() {
+        let a: u16 = 0b_1101_1111_0010;
+        let b: u16 = 0b_1_0101;
+        let res = byte_quotient(a, b);
+        let actual = 0b_1110_1011;
+        assert_eq!(res, actual);
+
+        let a: u16 = 0b_1_0101;
+        let b: u16 = 0b_1101_1111_0010;
+        let res = byte_quotient(a, b);
+        let actual = 0b_0;
+        assert_eq!(res, actual);
+    }
+
+    #[test]
+    fn byte_remainder_test() {
+        let a: u16 = 0b_1101_1111_0010;
+        let b: u16 = 0b_1_0101;
+        let res = byte_remainder(a, b);
+        let actual = 0b_101;
+        assert_eq!(res, actual);
+
+        let a: u16 = 0b_1_0101;
+        let b: u16 = 0b_1101_1111_0010;
+        let res = byte_remainder(a, b);
+        let actual = 0b_1_0101;
+        assert_eq!(res, actual);
+    }
+
+    #[test]
+    fn byte_inverse_test() {
+        let a: u8 = 1;
+        let res = byte_inverse(a);
+        let actual = 1;
+        assert_eq!(res, actual);
+
+        let a: u8 = 0b_110_1100;
+        let res = byte_inverse(a);
+        let actual = 0b_11_0011;
         assert_eq!(res, actual);
     }
 }
